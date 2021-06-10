428472445798a502aa5fca73c69bd0ba07814e32
U-D18\acarpenter <acarpenter@ACARPENTER-LT.D18.US.SWRI.ORG>
2021-06-10 18:29:23 -0500

    Homework 01 files commit

---
+++ homeworks/01/homework.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import sys
+import getopt
+
+def DoAges():
+
+    Ages = { 'Captain Picard':'55' , 'Dr. Crusher':'34' , 'Commander La Forge':'30' , 'Deeanna Troi':'36' }
+
+    f = open('StarTrekNames','r')
+
+    for line in f:
+        name = line.replace('\n','')
+        print('The age of ' + name + ' is ' + Ages[name])
+
+    f.close()
+
+if __name__ == '__main__':
+
+    DoAges()
